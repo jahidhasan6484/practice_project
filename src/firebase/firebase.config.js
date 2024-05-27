b2eba5b6b05f872ec0657a03bb57e5f164104bf9
JAHID <jahidhasan6484>
2024-05-28 03:24:32 +0600

    practice task done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArGvRnfgZcyOjz7ynubWGEixWGyF8cja4",
-  authDomain: "practice-task-e8dff.firebaseapp.com",
-  projectId: "practice-task-e8dff",
-  storageBucket: "practice-task-e8dff.appspot.com",
-  messagingSenderId: "382794074956",
-  appId: "1:382794074956:web:b8ba38b7a265da258d1de9",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
